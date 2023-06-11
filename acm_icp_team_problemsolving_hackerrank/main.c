6be6e843c58802946ceb57213f66291180ccb137
Bassem Saleh <bassem.alaa.edine@gmail.com>
2023-06-11 13:20:37 +0200

    Added ACM ICP team problem on Hackerrank problem solving Using C

---
+++ acm_icp_team_problemsolving_hackerrank/main.c
@@ -0,0 +1,210 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+int* acmTeam(int topic_count, char** topic, int* result_count) {
+    size_t topicNum = strlen(topic[0]);
+    int count = 0;
+    int max = 0;
+    int result = 0;
+    int itr, i, j;
+    for(i=0;i<topic_count-1;i++)
+    {
+        for(j=i+1;j<topic_count;j++)
+        {
+            for(itr =0;itr<topicNum;itr++)
+            {
+                if(topic[i][itr] == '1' || topic[j][itr]== '1')
+                {
+                    result++;
+                }
+            }
+            if(result>max)
+            {
+                max = result;
+                count = 1;
+            }
+            else if(result==max)
+            {
+                count ++;
+            }
+            result = 0;
+        }
+    }
+    *result_count = 2;
+    int *resultArr =  malloc(2 * sizeof(int));
+    resultArr[0] = max;
+    resultArr[1] = count;
+    return resultArr;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** first_multiple_input = split_string(rtrim(readline()));
+
+    int n = parse_int(*(first_multiple_input + 0));
+
+    int m = parse_int(*(first_multiple_input + 1));
+
+    char** topic = malloc(n * sizeof(char*));
+
+    for (int i = 0; i < n; i++) {
+        char* topic_item = readline();
+
+        *(topic + i) = topic_item;
+    }
+
+    int result_count;
+    int* result = acmTeam(n, topic, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%d", *(result + i));
+
+        if (i != result_count - 1) {
+            fprintf(fptr, "\n");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
+
